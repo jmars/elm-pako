653d2a42395d4b8869136e6585c4402864e58312
Jaye Marshall <jaye.marshall@beyondanalysis.net>
2015-06-04 16:15:50 +0100

    add some docs

--- src/Pako.elm
+++ src/Pako.elm
@@ -1,9 +1,13 @@
 module Pako where
 
+{-| Elm bindings to the [Pako[(http://nodeca.github.io/pako/)] compression library. -}
+
 import Native.Pako
 
+{-| Compress a string with deflate -}
 inflate : String -> String
 inflate = Native.Pako.inflate
 
+{-| Inflate a compressed string -}
 deflate : String -> String
 deflate = Native.Pako.deflate
